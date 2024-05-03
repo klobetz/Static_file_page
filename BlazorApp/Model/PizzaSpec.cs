08064252ff0357a18f7689bbd49db74ea2f622a1
Ákos Klobetz <klobetz@gmail.com>
2024-05-03 11:55:53 +0200

    pizzaSpec elkészítése és formázása

---
+++ BlazorApp/Model/PizzaSpec.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorApp.Model
+{
+    public class PizzaSpec
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public decimal BasePrice  { get; set; }
+        public string? ImageUrl { get; set; }
+        public string GetBasePrice() => BasePrice.ToString("0.00");
+    }
+}
